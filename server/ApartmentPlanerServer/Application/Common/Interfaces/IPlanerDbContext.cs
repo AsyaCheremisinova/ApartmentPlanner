76cfcdc8ae5da765e8dfedf7f2a38ebaa5529bf7
HungerGettingHard <talanov2001fortrash@gmail.com>
2023-04-26 14:19:02 +0300

    add server

---
+++ server/ApartmentPlanerServer/Application/Common/Interfaces/IPlanerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces
+{
+    public interface IPlanerDbContext
+    {
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
---
+++ server/ApartmentPlanerServer/Persistence/Repository/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using Persistence.DbContext;
+using Domain.Entities;
+using Application.Common.Interfaces;
+using System;
+
+namespace Persistence.Repository
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly IPlannerDbContext _context;
+        private GenericRepository<Request> _requestRepository;
+        private GenericRepository<Status> _statusRepository;
+
+
+
+        public UnitOfWork(IPlannerDbContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<Request> GenreRepository => _requestRepository ??= new GenericRepository<Request>(_context);
+        public IGenericRepository<Status> StatusRepository => _statusRepository ??= new GenericRepository<Status>(_context);
+
+
+        #region Dispose
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
